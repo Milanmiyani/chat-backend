78de4f7f955470af738da30379a595c48fdb3e46
Milan Miyani <milanmiyani2303@gmail.com>
2025-06-13 13:03:28 +0530

    adding vercel json

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+// ...existing code...
+app.get('/', (req, res) => {
+  res.send('Backend API is running');
+});
+// ...existing code...
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
